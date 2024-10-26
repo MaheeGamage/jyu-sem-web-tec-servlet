ffc30597859965ab7a9ec573eeaa0dd1210e80c3
MaheeGamage <mahee.gamage@gmail.com>
2024-10-26 04:04:24 +0300

    FE updated to input assignment data

--- src/main/webapp/myJS.js
+++ src/main/webapp/myJS.js
@@ -1,13 +1,19 @@
 function doQuery()
-{
-//alert('doQuery...');	
-	if((document.getElementById('tf01').value!='')&&(document.getElementById('tf02').value!='')&&(document.getElementById('tf03').value!=''))
+{	
+	if((document.getElementById('inpName').value!='')&&(document.getElementById('inpPeopleCount').value!='')&&(document.getElementById('inpBedroomCount').value!=''))
 	{
 		var q_str = 'reqType=doQuery';
 
-		q_str = q_str+'&param01='+document.getElementById('tf01').value;
-		q_str = q_str+'&param02='+document.getElementById('tf02').value;
-		q_str = q_str+'&param03='+document.getElementById('tf03').value;
+		q_str = q_str+'&name='+document.getElementById('inpName').value;
+		q_str = q_str+'&peopleCount='+document.getElementById('inpPeopleCount').value;
+		q_str = q_str+'&bedroomCount='+document.getElementById('inpBedroomCount').value;
+		q_str = q_str+'&maxLakeDistance='+document.getElementById('inpMaxLakeDistance').value;
+		q_str = q_str+'&city='+document.getElementById('inpCity').value;
+		q_str = q_str+'&maxCityDistance='+document.getElementById('inpMaxCityDistance').value;
+		q_str = q_str+'&dayCount='+document.getElementById('inpDayCount').value;
+		q_str = q_str+'&startDate='+document.getElementById('inpStartDate').value;
+		q_str = q_str+'&maxDayShifts='+document.getElementById('inpMaxDayShifts').value;
+		
 		doAjax('Booking',q_str,'doQuery_back','post',0);
 	}else
 	{
